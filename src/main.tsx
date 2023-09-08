65b016b8a435c494516b3fa19a02165ab1a50cd0
yp4kfrday <dstrctnex@gmail.com>
2023-09-08 13:29:38 +0400

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-
+import React from 'react';
 import App from './app/app';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const getOrCreateRootElement = (Id: string) => {
+  const result = document.getElementById(Id);
+
+  if (result) {
+    return result;
+  }
+
+  const div = document.createElement("div");
+  div.id = Id;
+
+  document.body.appendChild(div);
+
+  return div;
+}
+
+const rootElement = getOrCreateRootElement("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
