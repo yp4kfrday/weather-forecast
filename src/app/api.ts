bd9868337b5e27858df412af4d28c2a21fd7d0ff
yp4kfrday <dstrctnex@gmail.com>
2023-09-10 21:27:04 +0400

    feat: add search component and api

---
+++ src/app/api.ts
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '08bb366217msha091fb19fff975ap12258ejsnd8025a632743',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
